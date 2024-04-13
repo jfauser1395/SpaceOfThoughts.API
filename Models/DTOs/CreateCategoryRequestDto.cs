5267962b3eaaa645f5b2124c972f087815258218
jurifauser1395 <jurifauser@pm.me>
2024-04-13 14:53:26 +0200

    Add project files.

---
+++ Models/DTOs/CreateCategoryRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Artblog.API.Models.DTOs
+{
+    public class CreateCategoryRequestDto
+    {
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+
+    }
+}
---
+++ Models/Domain/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace Artblog.API.Models.Domain
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+
+
+    }
+}
